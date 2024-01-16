c89148e3ee3966aaf2cdbeb1f09c3d2961b2c31c
I Gusti Ayu Nirmala Dewi <153574859+nirmala0104@users.noreply.github.com>
2024-01-16 21:50:34 +0800

    penambahan fungsi tampilan
    membuat fungsi yang menampilkan jumlah karyawan

--- distribusi_uang.cpp
+++ distribusi_uang.cpp
@@ -7,10 +7,15 @@ using namespace std;
 
 //----kode di atas batas ini tidak boleh diganti atau ditambah----
 
+const int karyawanMaks = 100;
 
-void tampil_jml_karyawan(){
+void tampil_jml_karyawan(long long distribusiSekarang[],int index){
 
-//CODE
+cout << "Output:"<<index<<" orang\n";
+for (int i=0; i<index; ++i){
+    cout << "Karyawan"<<i+1<<":Rp."<<distribusiSekarang[i]<<endl;
+}
+exit(0)
 }
 
 int main(){
